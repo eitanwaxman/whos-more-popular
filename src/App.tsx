5cf60aa58184e5c371c378898f6b2e8ca9f516f3
langroops <langroops@gmail.com>
2022-06-26 18:47:31 -0400

    facebook sdk attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    FB.getLoginStatus(function (response) {
+      statusChangeCallback(response);
+      console.log(response);
+    });
+  }, []);
+
   return (
     <>
       <div className="login">
